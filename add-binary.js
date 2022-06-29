d3c258643fe1614f2851f7784a8c9f6fa2102671
Asmita Gaire <asmitagaire00@gmail.com>
2022-06-29 10:21:47 +1000

    change the solution

--- add-binary.js
+++ add-binary.js
@@ -1,6 +1,22 @@
 // Qn: https://leetcode.com/problems/add-binary/
 
-const addBinary =  (a,b)=>{
-    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);   
-}
-console.log(addBinary(11,1));
+const addBinary = (a, b) => {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0,
+    res = "";
+
+  for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+    let sum = (parseInt(a[i]) || 0) + (parseInt(b[j]) || 0) + carry;
+    if (sum > 1) {
+      sum = sum % 2;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    res = `${sum}${res}`;
+  }
+  return res;
+};
+
+console.log(addBinary("1010", "1011"));
