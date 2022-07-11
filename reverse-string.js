c48168a4716de89ff5a7952fcd22f9aca405bbf8
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-11 12:01:56 +1000

    solve

---
+++ reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {word[]} s
+ * @returns {word[]}
+ */
+
+const reverseString = (s) => {
+  console.log(s.reverse());
+};
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
