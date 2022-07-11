78c3f8d0e8a79a25453e0a3caa54d9135ac3aa33
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-11 11:28:06 +1000

    solve

---
+++ best-time-to-buy-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param {number[]} prices
+ * @returns {number}
+ */
+
+const maxProfit = (prices) => {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i - 1] < prices[i]) {
+      console.log(prices[i], prices[i - 1]);
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return profit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
