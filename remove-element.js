1252dd75cf8705beda39d2aa7ca7a170ddefb80b
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-28 11:33:50 +1000

    solve

---
+++ remove-element.js
@@ -0,0 +1,18 @@
+//Qn:https://leetcode.com/problems/remove-element/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+};
+console.log(removeElement([3, 2, 2, 3], 3));
